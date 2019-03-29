25e02fb10916daa23130de828ed88586e0f6139f
Marius Colacioiu <marius.colacioiu@gmail.com>
2019-03-29 22:46:42 +0100

    Remove solution code

--- index.js
+++ index.js
@@ -42,30 +42,7 @@ const typeDefs = gql`
 // Provide resolver functions for your schema fields
 const resolvers = {
   Query: {
-    search: (source, args, context, info) => {
-      const { query } = args;
-      // go to elastic and search for the product ids
-      const result = db.searchIndex[query];
-
-      return result;
-    }
   },
-  SearchResult: {
-    products: (source, args) => {
-      const ids = source;
-      const resolvedProducts = db.products.filter(p => ids.indexOf(p.id) >= 0);
-
-      return {
-        total: resolvedProducts.length,
-        results: resolvedProducts,
-        hasMore: false
-      };
-    }
-  },
-  SortOrder: {
-    Ascending: 0,
-    Descending: 1
-  }
 };
 
 const server = new ApolloServer({
